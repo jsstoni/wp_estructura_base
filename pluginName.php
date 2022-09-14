7ef2088ffbfb427b6bc83ec7387fb19154c54b53
jsstoni <jsstoniha@gmail.com>
2022-09-14 15:52:16 -0300

    estructura base del plugin wordpress

---
+++ pluginName.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: Nombre para el plugin de WordPress
+* Plugin URI: https://miweb.com
+* Description: Añade una descripción para el plugins de WordPress
+* Author: Nombre de autor
+* Author URI: https://miweb.com
+* Version: 1.0
+* License: GPLv2
+* Text Domain: pluginName
+*/
