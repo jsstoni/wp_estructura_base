60b30d2110948cf985b4b4e4190939c5429e4b40
jsstoni <jsstoniha@gmail.com>
2022-09-18 06:17:26 -0300

    _

--- includes/pluginName.class.php
+++ includes/pluginName.class.php
@@ -3,12 +3,18 @@ class pluginName
 {
 	public function __construct()
 	{
+		$this->load_plugin_textdomain();
 		$this->includes();
 		$this->define_hooks();
 	}
 
 	public function active()
 	{
+	}
+
+	public function load_plugin_textdomain()
+	{
+		load_plugin_textdomain( 'pluginName', false, basename( dirname( __FILE__ ) ) . '/lang/' );
 	}
 
 	public function includes()
